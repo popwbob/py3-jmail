6fbddf58d322a1fe664253e3d72dd515b780ec5e
jrms <git@jrms.com.ar>
2014-12-14 22:40:47 -0300

    remove database from code source

--- jmail/settings.py
+++ jmail/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'jmail.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '/opt/jmail/db.sqlite3'),
     }
 }
 
