0e18f1924af67eb10c6ad5c1c3cf4cd8dfbb2d5e
jrms <git@jrms.com.ar>
2015-01-23 13:01:52 -0300

    improve urls regexs

--- jmail/mail/urls.py
+++ jmail/mail/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^(\d+)/([^\/]+)/(\d+)/source/$', 'jmail.mail.views.source', name='source'),
-
-    #~ url(r'^(\d+)/([a-zA-Z0-9.\-_]+)/(\d+)/$', 'jmail.mail.views.read', name='read'),
-    url(r'^(\d+)/([^\/]+)/(\d+)/read/$', 'jmail.mail.views.read', name='read'),
+    url(r'^(\d+)/([\w=]+)/(\d+)/source/$', 'jmail.mail.views.source', name='source'),
+    url(r'^(\d+)/([\w=]+)/(\d+)/read/$', 'jmail.mail.views.read', name='read'),
 )
