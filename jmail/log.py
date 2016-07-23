c404c804c39b3035282e4838b2110d492432e7ed
Jeremías Casteglione <git@jrms.com.ar>
2016-07-23 15:49:11 -0300

    set outfile from constructor

--- jmail/log.py
+++ jmail/log.py
@@ -2,7 +2,10 @@ import sys
 
 class JMailLog:
     _tag = 'JMail'
-    _outfile = sys.stderr
+    _outfile = None
+
+    def __init__(self, outfile=sys.stderr):
+        self._outfile = outfile
 
     def _log(self, tag, *line_items):
         print(self._tag, '[{}] - '.format(tag), sep='', end='', file=self._outfile)
