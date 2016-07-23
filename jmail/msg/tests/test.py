c095af8a2d6f5aef1d1e9f7060a87a099e3b3a77
Jeremías Casteglione <git@jrms.com.ar>
2016-07-23 17:52:34 -0300

    test message short flags

--- jmail/msg/tests/test.py
+++ jmail/msg/tests/test.py
@@ -16,6 +16,7 @@ class TestMsg(JMailTest):
         self.assertIsInstance(repr(m), str)
         self.assertIsNone(m.headers)
         self.assertIsInstance(m.flags, tuple)
+        self.assertEqual(str(m), '(.headers=None)')
 
     def test_message_flags(self):
         m = JMailMessage(meta=b'32 (UID 316 FLAGS ())')
@@ -35,6 +36,22 @@ class TestMsg(JMailTest):
         self.assertTupleEqual(m.flags, (b'\\Other', b'\\Flags'))
         self.assertFalse(m.seen)
 
+    def test_message_flags_short(self):
+        m = JMailMessage(meta=b'32 (UID 316 FLAGS ())')
+        self.assertIsInstance(m.flags_short, str)
+        self.assertEqual(m.flags_short, '')
+
+    def test_message_flags_short_attach(self):
+        m = JMailMessage(meta=b'32 (UID 316 FLAGS ())')
+        m.attachs = ['fake1', 'fake2']
+        s = m._flags_short(m.flags)
+        self.assertEqual(s, 'A')
+
+    def test_message_flags_short_answered(self):
+        m = JMailMessage(meta=b'32 (UID 316 FLAGS (\\Answered))')
+        self.assertEqual(m.flags_short, 'R')
+
     def test_zzz_cleanup_message(self):
+        """this should be always the last one"""
         JMailBase.conf = None
         JMailBase.log = None
