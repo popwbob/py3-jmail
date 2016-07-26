5e500715ff088735bb55ff733efc300a8e09a2da
Jeremías Casteglione <git@jrms.com.ar>
2016-07-26 01:59:53 -0300

    test email msg attachs

--- jmail/msg/tests/test_mailmsg.py
+++ jmail/msg/tests/test_mailmsg.py
@@ -35,12 +35,39 @@ Content-Type: multipart/alternative; boundary="----=_Part"
 Content-Type: text/plain; charset=ISO-8859-1
 Content-Transfer-Encoding: quoted-printable
 
-plain text body part
+plain text body
 ------=_Part
 Content-Type: text/html; charset=ISO-8859-1
 Content-Transfer-Encoding: quoted-printable
 
-<body>html part</body>
+<body>html</body>
+------=_Part--
+'''.encode()
+
+
+eml_mpart_attach = '''X-Test: jmail
+Subject: html email
+Mime-Version: 1.0
+Content-Type: multipart/alternative; boundary="----=_Part"
+
+------=_Part
+Content-Type: text/plain; charset=US-ASCII
+Content-Transfer-Encoding: 7bit
+Content-Disposition: inline
+
+plain text body
+------=_Part
+Content-Type: application/octet-stream
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment; filename=jmail-attach.test
+
+aG9sYSBtdW5kbyEK
+------=_Part
+Content-Type: application/octet-stream
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment; filename=jmail-attach.ignored
+
+aG9sYSBtdW5kbyEK
 ------=_Part--
 '''.encode()
 
@@ -58,6 +85,15 @@ class TestMailMsg(JMailTest):
         self.assertEqual(m.payload(), b'Mundo!\n')
         self.assertEqual(m.size_human(), '36.00B')
 
+    def test_mailmsg_source(self):
+        m = JMailMessage(source=eml_mpart)
+        sl = m.source_lines()
+        self.assertEqual(len(sl), 11)
+        self.assertEqual(sl[0], 'X-Test: jmail')
+        self.assertEqual(sl[1], 'Subject: En el =?unknown-8bit?q?R=EDo?=')
+        self.assertEqual(sl[9], 'Que direcci=F3n?')
+        self.assertEqual(sl[10], '------=_Part--')
+
     def test_mailmsg_mpart_lines(self):
         m = JMailMessage(source=eml_mpart)
         subj = m.headers.get('Subject')
@@ -68,16 +104,18 @@ class TestMailMsg(JMailTest):
 
     def test_mailmsg_mpart_html(self):
         m = JMailMessage(source=eml_mpart_html)
-        self.assertEqual(m.body_html(), '<body>html part</body>')
+        self.assertEqual(m.body_html(), '<body>html</body>')
+        self.assertIsNone(m.payload())
 
-    def test_mailmsg_source(self):
-        m = JMailMessage(source=eml_mpart)
-        sl = m.source_lines()
-        self.assertEqual(len(sl), 11)
-        self.assertEqual(sl[0], 'X-Test: jmail')
-        self.assertEqual(sl[1], 'Subject: En el =?unknown-8bit?q?R=EDo?=')
-        self.assertEqual(sl[9], 'Que direcci=F3n?')
-        self.assertEqual(sl[10], '------=_Part--')
+    def test_mailmsg_mpart_attach(self):
+        m = JMailMessage(source=eml_mpart_attach)
+        self.assertListEqual(m.body_lines(), ['plain text body'])
+        p = m.parts()
+        self.assertEqual(len(p), 3)
+        self.assertEqual(p[1].payload(), b'hola mundo!\n')
+        self.assertEqual(p[1].filename(), 'jmail-attach.test')
+        self.assertEqual(p[1].filename_encode(), 'am1haWwtYXR0YWNoLnRlc3Q=')
+        self.assertEqual(p[1].size_human(), '150.00B')
 
     # this should be always the last one
     def test_zzz_cleanup_message(self):
