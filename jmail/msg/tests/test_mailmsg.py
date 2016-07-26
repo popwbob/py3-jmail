f0a9c8fa53620f805f4f1678cfe1c77bdc80d8e4
Jeremías Casteglione <git@jrms.com.ar>
2016-07-26 01:27:49 -0300

    test mail msg source

--- jmail/msg/tests/test_mailmsg.py
+++ jmail/msg/tests/test_mailmsg.py
@@ -5,14 +5,8 @@ from jmail.log import JMailLog
 from jmail import JMailBase
 from django.conf import settings
 
-class TestMailMsg(JMailTest):
 
-    def setUp(self):
-        JMailBase.conf = settings.JMAIL.copy()
-        JMailBase.log = JMailLog(outfile=StringIO())
-
-    def test_mailmsg(self):
-        d = '''X-Test: jmail
+em_mpart_enc = '''X-Test: jmail
 Subject: En el =?unknown-8bit?q?R=EDo?=
 Mime-Version: 1.0
 Content-Type: multipart/alternative; boundary="----=_Part1"
@@ -22,13 +16,32 @@ Content-Type: text/plain; charset=ISO-8859-1
 Content-Transfer-Encoding: quoted-printable
 
 Que direcci=F3n?
-'''
-        m = JMailMessage(source=d.encode())
+------=_Part1--
+'''.encode()
+
+
+class TestMailMsg(JMailTest):
+
+    def setUp(self):
+        JMailBase.conf = settings.JMAIL.copy()
+        JMailBase.log = JMailLog(outfile=StringIO())
+
+    def test_mailmsg(self):
+        m = JMailMessage(source=em_mpart_enc)
         subj = m.headers.get('Subject')
         self.assertTrue(subj.startswith('En el R'))
         self.assertTrue(subj.endswith('o'))
         self.assertEqual(ord(subj[7]), 65533)
         self.assertListEqual(m.body_lines(), [r'Que dirección?'])
+
+    def test_mailmsg_source(self):
+        m = JMailMessage(source=em_mpart_enc)
+        sl = m.source_lines()
+        self.assertEqual(len(sl), 11)
+        self.assertEqual(sl[0], 'X-Test: jmail')
+        self.assertEqual(sl[1], 'Subject: En el =?unknown-8bit?q?R=EDo?=')
+        self.assertEqual(sl[9], 'Que direcci=F3n?')
+        self.assertEqual(sl[10], '------=_Part1--')
 
     # this should be always the last one
     def test_zzz_cleanup_message(self):
